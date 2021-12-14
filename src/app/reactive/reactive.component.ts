dc52ff6a32647871ba41387a0ced54c008605174
Mirjam Thomet <mirjamt@inova.ch>
2021-12-14 15:12:12 +0100

    code review

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,35 +1,121 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'reactive',
   templateUrl: './reactive.component.html',
-  styleUrls: ['./reactive.component.scss']
+  styleUrls: ['./reactive.component.scss'],
 })
 export class ReactiveComponent implements OnInit {
-
-  constructor(private fb: FormBuilder) { }
-
-  ngOnInit(): void {
-  }
   public sizes: string[] = ['Small', 'Medium', 'Large'];
 
   //FormControls
-  profileForm = this.fb.group({
-    vorname: new FormControl('keine Zahlen, nur -', [Validators.pattern('[-a-zA-Z ]*'), Validators.required]), //['keine Zahlen, nur -', [Validators.pattern('[-a-zA-Z ]*'), Validators.required]],
-    nachname: ['keine Zahlen, nur -', [Validators.pattern('[-a-zA-Z ]*'), Validators.required]],
-    street1: ['required', Validators.required],
-    postcode1: ['4 zahlen', [Validators.required, Validators.pattern('[0-9]{4}')]],
-    location1: ['keine zahlen', [Validators.required, Validators.pattern('[A-Za-z]*')]],
-    size: ['', Validators.required],
-    street2: ['', [Validators.required]],
-    postcode2: ['', [Validators.required, Validators.pattern('[0-9]{4}')]],
-    location2: ['keine zahlen', [Validators.required, Validators.pattern('[A-Za-z]*')]],
-    // password: ['', Validators.required],
-    // email: ['', Validators.required]
-  });
+  profileForm = this.fb.group(
+    {
+      vorname: new FormControl('keine Zahlen, nur -', [
+        Validators.pattern('[-a-zA-Z ]*'),
+        Validators.required,
+      ]), //['keine Zahlen, nur -', [Validators.pattern('[-a-zA-Z ]*'), Validators.required]],
+      nachname: [
+        'keine Zahlen, nur -',
+        [Validators.pattern('[-a-zA-Z ]*'), Validators.required],
+      ],
+      street1: ['required', Validators.required],
+      postcode1: [
+        '4 zahlen',
+        [Validators.required, Validators.pattern('[0-9]{4}')],
+      ],
+      location1: [
+        'keine zahlen',
+        [Validators.required, Validators.pattern('[A-Za-z]*')],
+      ],
+      size: ['', Validators.required],
+      street2: ['', [Validators.required]],
+      postcode2: ['', [Validators.required, Validators.pattern('[0-9]{4}')]],
+      location2: [
+        'keine zahlen',
+        [Validators.required, Validators.pattern('[A-Za-z]*')],
+      ],
+      password1: ['', Validators.required],
+      password2: ['', Validators.required],
+      email: ['', Validators.required],
+    },
+    { validators: this.passwordValidation() }
+  );
 
+  get vorname() {
+    return this.profileForm.get('vorname');
+  }
 
-  get vorname() { return this.profileForm.get('vorname'); }
+  get nachname() {
+    return this.profileForm.get('nachname');
+  }
 
+  get street1() {
+    return this.profileForm.get('street1');
+  }
+
+  get postcode1() {
+    return this.profileForm.get('postcode1');
+  }
+
+  get location1() {
+    return this.profileForm.get('location1');
+  }
+
+  get size() {
+    return this.profileForm.get('size');
+  }
+
+  get street2() {
+    return this.profileForm.get('street2');
+  }
+
+  get postcode2() {
+    return this.profileForm.get('postcode2');
+  }
+  get location2() {
+    return this.profileForm.get('location2');
+  }
+
+  get password1() {
+    return this.profileForm.get('password1');
+  }
+
+  get password2() {
+    return this.profileForm.get('password2');
+  }
+
+  get email() {
+    return this.profileForm.get('email');
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    console.log(this.vorname?.valid);
+  }
+
+  /** Validation when passwords dont match */
+  private passwordValidation() {
+    return (form: FormGroup) => {
+      const password1 = form.get('password1');
+      const password2 = form.get('password2');
+      const error =
+        password1?.value !== password2?.value
+          ? { passwordsDontMatch: true }
+          : null;
+      password1?.setErrors(error); //<--see the setErrors
+      password2?.setErrors(error); //<--see the setErrors
+      return error;
+    };
+  }
 }
--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -1,59 +1,77 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { AbstractControl, NgForm, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import {
+  AbstractControl,
+  NgForm,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
 import { Adress, Person } from '../models';
 
 @Component({
   selector: 'template-driven',
   templateUrl: './template-driven.component.html',
-  styleUrls: ['./template-driven.component.scss']
+  styleUrls: ['./template-driven.component.scss'],
 })
-export class TemplateDrivenComponent implements OnInit {
-
+export class TemplateDrivenComponent {
+  //TODO: Kommentar fehlt
   @ViewChild('form')
   public form: NgForm;
 
-  constructor() { }
-
+  //TODO: Kommentar fehlt
   public submitted = false;
+  //TODO: Kommentar fehlt
   public sameAdress: Boolean = false;
+  //TODO: Kommentar fehlt
   public secondAdress: Adress = {
     street: 'Bernstrasse 5',
     postCode: '3072',
-    location: 'Ostermundigen'
+    location: 'Ostermundigen',
   };
+  //TODO: Kommentar fehlt
   public sizes: string[] = ['Small', 'Medium', 'Large'];
 
   //passwörter
   public pw1: string = '';
+  //TODO: Kommentar fehlt
   public pw2: string = '';
 
-  public modelFilled: Person = new Person('von Wartburg', 'Gian',
+  //TODO: Kommentar fehlt
+  public modelFilled: Person = new Person(
+    'von Wartburg',
+    'Gian',
     {
       street: 'Wiesenstrasse',
       postCode: '3014',
-      location: 'Bern'
-    }, '', 'gianvonwartburg@gmail.com', '', '');
+      location: 'Bern',
+    },
+    '',
+    'gianvonwartburg@gmail.com',
+    '',
+    ''
+  );
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  //TODO: Docstring fehlt
   public onSubmit(): void {
     this.submitted = true;
   }
 
+  //TODO: Docstring fehlt
   public onCheck(): void {
     this.sameAdress = !this.sameAdress;
   }
 
+  //TODO: Docstring fehlt
   public comparePasswords() {
     if (this.pw2 === this.modelFilled.password) {
-      console.log('hallo')
+      console.log('hallo');
       return;
     } else {
-      console.log('hallo2')
+      console.log('hallo2');
 
-      this.form.form.controls['passwort'].setErrors({ 'incorrect': true });
-      this.form.form.controls['passwort2'].setErrors({ 'incorrect': true });
+      this.form.form.controls['passwort'].setErrors({ incorrect: true });
+      this.form.form.controls['passwort2'].setErrors({ incorrect: true });
     }
   }
 }
